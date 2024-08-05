4154d1ba2cbc2c64a52da2714b93f83d3e9b3da0
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-08-05 22:22:33 +0530

    added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import './App.css'
-import { UserProvider } from "./lib/context/user"
+import { UserProvider,useUser } from "./lib/context/user"
 
 function App() {
 
@@ -11,9 +11,32 @@ function App() {
   return (
     <div>
       <UserProvider>
+        <Navbar />
          <main>{isLoginPage ? <Login /> : <Home />}</main>
       </UserProvider>
     </div>
+  )
+}
+
+function Navbar() {
+  const user =useUser();
+
+  return (
+    <nav>
+      <a href="/">Idea Tracker</a>
+      <div>
+        {user.current ? (
+          <>
+            <span>{user.current.e}</span>
+            <button type='button' onClick={() => user.logout()}>
+              Logout
+            </button>
+          </>
+        ):(
+          <a href="/login">Login</a>
+        )}
+      </div>
+    </nav>
   )
 }
 
