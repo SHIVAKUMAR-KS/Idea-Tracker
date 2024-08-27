ccc3bd91190008c145bc0ca355664e016ab38342
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-08-27 15:48:02 +0530

    all has done but lacking with  collection id

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,23 @@
-import { useState } from 'react'
-import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
-import './App.css'
-import { UserProvider,useUser } from "./lib/context/user"
-import { IdeasProvider } from './lib/context/ideas';
-
+import { Home } from "./pages/Home";
+import { UserProvider } from "./lib/context/user";
+import { IdeaProvider } from "./lib/context/ideas";
 
 function App() {
-
   const isLoginPage = window.location.pathname === "/login";
 
   return (
     <div>
       <UserProvider>
-        <IdeasProvider>
-          <Navbar />
+
+        <IdeaProvider>
+          <Navbar /> {/* Add the navbar before page content */}
           <main>{isLoginPage ? <Login /> : <Home />}</main>
-
-        </IdeasProvider>
-        
+        </IdeaProvider>
       </UserProvider>
+     \
     </div>
-  )
+  );
 }
 
-function Navbar() {
-  const user =useUser();
-
-  return (
-    <nav>
-      <a href="/">Idea Tracker</a>
-      <div>
-        {user.current ? (
-          <>
-            <span>{user.current.e}</span>
-            <button type='button' onClick={() => user.logout()}>
-              Logout
-            </button>
-          </>
-        ):(
-          <a href="/login">Login</a>
-        )}
-      </div>
-    </nav>
-  )
-}
-
-export default App
+export default App;
--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -3,7 +3,7 @@ import { Client, Databases, Account } from "appwrite";
 const client = new Client();
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(process.env.project_id); // Replace with your project ID
+  .setProject("66adfeb50023c740838a"); // Replace with your project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
